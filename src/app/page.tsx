a0766a06df5c2ad39628129ccb872b7b556be705
MAC <ronyortiz@MACs-iMac.local>
2025-02-18 22:22:05 -0400

    IC Next setup + TailwindCSS v4 & ShadCN

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-blue-600 font-semibold">hello abundance</h1>
+      <Button>Testing button</Button>
+    </div>
+  );
+}
