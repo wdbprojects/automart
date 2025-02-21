974500773c9aa23cf606655986ec67ab2039f30d
MAC <ronyortiz@MACs-iMac.local>
2025-02-21 19:56:28 -0400

    all seeding proceses complete, ready to start the UI

---
+++ prisma/seed/classified.seeds.ts
@@ -0,0 +1,77 @@
+import { faker } from "@faker-js/faker";
+import {
+  BodyType,
+  ClassifiedStatus,
+  Color,
+  Currency,
+  FuelType,
+  OdoUnit,
+  Prisma,
+  PrismaClient,
+  Transmission,
+} from "@prisma/client";
+import slug from "slug";
+
+export async function seedClassifieds(prisma: PrismaClient) {
+  const makes = await prisma.make.findMany({
+    include: { models: { include: { modelVariants: true } } },
+  });
+
+  const classifiedsData: Prisma.ClassifiedCreateManyInput[] = [];
+
+  for (let i = 0; i < 25; i++) {
+    const make = faker.helpers.arrayElement(makes);
+    if (!make.models.length) continue;
+    const model = faker.helpers.arrayElement(make.models);
+    const variant = model.modelVariants.length
+      ? faker.helpers.arrayElement(model.modelVariants)
+      : null;
+
+    console.log("MAKE:", make);
+    console.log("MODEL:", model);
+    console.log("VARIANT:", variant);
+
+    const year = faker.date
+      .between({
+        from: new Date(1925, 0, 1),
+        to: new Date(),
+      })
+      .getFullYear();
+
+    const title = [year, make.name, model.name, variant?.name]
+      .filter(Boolean)
+      .join("");
+    const vrm = faker.vehicle.vrm();
+    const baseSlug = slug(`${title}-${vrm}`);
+
+    classifiedsData.push({
+      year: year,
+      vrm: vrm,
+      slug: baseSlug,
+      makeId: make.id,
+      modelId: model.id,
+      ...(variant?.id && { modelVariantId: variant.id }),
+      title: title,
+      price: faker.number.int({ min: 40000, max: 1000000 }),
+      odoReading: faker.number.int({ min: 0, max: 200000 }),
+      doors: faker.number.int({ min: 2, max: 8 }),
+      seats: faker.number.int({ min: 2, max: 8 }),
+      views: faker.number.int({ min: 0, max: 10000 }),
+      description: faker.commerce.productDescription(),
+      status: faker.helpers.arrayElement(Object.values(ClassifiedStatus)),
+      color: faker.helpers.arrayElement(Object.values(Color)),
+      currency: faker.helpers.arrayElement(Object.values(Currency)),
+      odoUnit: faker.helpers.arrayElement(Object.values(OdoUnit)),
+      bodyType: faker.helpers.arrayElement(Object.values(BodyType)),
+      transmission: faker.helpers.arrayElement(Object.values(Transmission)),
+      fuelType: faker.helpers.arrayElement(Object.values(FuelType)),
+    });
+  }
+
+  const result = await prisma.classified.createMany({
+    data: classifiedsData,
+    skipDuplicates: true, // skip any duplicate errors from duplicate slugs
+  });
+
+  console.log(`Classifieds seeded total of ${result.count} results`);
+}
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 import { seedTaxonomy } from "./taxonomy.seed";
+import { seedClassifieds } from "./classified.seeds";
+import { seedImages } from "./images.seeds";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
-  await seedTaxonomy(prisma);
+  // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
+  // await seedTaxonomy(prisma);
+  // await seedClassifieds(prisma);
+  await seedImages(prisma);
 }
-
 main()
   .catch((err) => {
     throw err;
