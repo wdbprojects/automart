745011eb632b52baae3d62129c9d4b17291f2108
iMac <ronyortiz@iMacs-iMac.local>
2025-05-23 00:45:37 -0400

    IC Installed & Configured Next.js + TailwindCSS v4

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import DarkMode from "@/components/shared/dark-mode";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to love and abundance</h2>
+      <Button>Testing ShadCN</Button>
+      <DarkMode />
+    </div>
+  );
+}
