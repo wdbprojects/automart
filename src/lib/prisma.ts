2beac05b006f78b5348debb0cb3ff7cade04e2d2
iMac <ronyortiz@iMacs-iMac.local>
2025-05-23 22:13:48 -0400

    setup prisma and local DB + all schemas

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+function makeClient() {
+  return PrismaClient({ log: ["error", "info", "warn"] });
+}
+
+const prisma = globalForPrisma.prisma || makeClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
