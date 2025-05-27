78c2bca138d6f75a35fc66b7fb959806038fe1df
iMac <ronyortiz@iMacs-iMac.local>
2025-05-26 21:24:22 -0400

    seed database with classified data

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { seedTaxonomy } from "./taxonomy.seed";
+import { seedClassifieds } from "./classified.seed";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
-  await seedTaxonomy(prisma);
+  // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
+  // await seedTaxonomy(prisma);
+  await seedClassifieds(prisma);
 };
 
 main()
